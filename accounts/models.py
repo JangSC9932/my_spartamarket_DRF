d473706357ca678510e6970b03b11da5e05b1ca5
장석천 <jangseogcheon@jangseogcheon-ui-MacBookPro.local>
2024-04-30 19:29:03 +0900

    계정 생성 API 완료

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=10, blank=False)
+    last_name = models.CharField(max_length=10, blank=False)
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=30)
+    gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')), null=True)
+    birthday = models.DateField()
+    introduction = models.TextField(null=True)
