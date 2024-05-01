8ce5638b62b64051e5cc50566c66df932428d85c
장석천 <jangseogcheon@jangseogcheon-ui-MacBookPro.local>
2024-05-01 14:42:41 +0900

    products 필수기능 완료

--- spartamarket_DRF/settings.py
+++ spartamarket_DRF/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     # local
     'accounts',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -136,6 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
