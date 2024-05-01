3b52770b0a1ed152dba01a1baa8ec55feed7f35b
장석천 <jangseogcheon@jangseogcheon-ui-MacBookPro.local>
2024-05-01 21:07:02 +0900

    account response 수정

--- accounts/views.py
+++ accounts/views.py
@@ -8,21 +8,32 @@ from .serializers import UserSerializer
 
 class AccountsAPIView(APIView):
 
-    @staticmethod
-    def post(request):
+    def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response("회원가입 성공", status=status.HTTP_201_CREATED)
 
-    @staticmethod
-    def get(request, username):
+            response = {
+                'code': status.HTTP_201_CREATED,
+                'message': 'Registered successfully',
+                'data': serializer.data,
+            }
+
+            return Response(response, status=status.HTTP_201_CREATED)
+
+    def get(self, request, username):
         if request.user.is_authenticated:
             user = User.objects.filter(username=username).first()
             serializer = UserSerializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response("로그인이 필요합니다.", status=status.HTTP_401_UNAUTHORIZED)
+
+            response = {
+                'code': status.HTTP_401_UNAUTHORIZED,
+                'message': 'LOGIN REQUIRED',
+            }
+
+            return Response(response, status=status.HTTP_401_UNAUTHORIZED)
 
 
 
